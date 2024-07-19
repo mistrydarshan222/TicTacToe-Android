17de3961b6e075db10bab9cfd1a433abc55930a5
mistrydarshan222 <69639961+mistrydarshan222@users.noreply.github.com>
2024-07-19 12:30:28 -0400

    QUit Button Updated

--- app/src/main/java/com/example/assignment2_connect3/view/ConnectThree.java
+++ app/src/main/java/com/example/assignment2_connect3/view/ConnectThree.java
@@ -60,7 +60,10 @@ public class ConnectThree extends AppCompatActivity {
 
         quitButton.setOnClickListener(view -> {
             congratsTextView.clearAnimation(); // Stop the animation
-            finishAffinity(); // Close the app
+            Intent intent = new Intent(ConnectThree.this, GameStart.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+            finish(); // Finish the current activity
         });
 
         // Observe game state changes
